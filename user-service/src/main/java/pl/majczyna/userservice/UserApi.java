c61114b8a9c6af79c416cd9b6e950bf76084198d
RobertMacbook <robertmajczyna1989@gmail.com>
2024-01-24 08:32:51 +0100

    add add zuul service

---
+++ user-service/src/main/java/pl/majczyna/userservice/UserApi.java
@@ -0,0 +1,13 @@
+package pl.majczyna.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApi {
+    @GetMapping()
+    public String hello(){
+
+        return "hello world from UserApi!";
+    }
+}
