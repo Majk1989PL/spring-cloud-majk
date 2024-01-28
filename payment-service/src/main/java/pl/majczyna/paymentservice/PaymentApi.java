dabb2ad51a993851dc3a1805c30a1d4ea2569a4d
Robert <robertmajczyna1989@gmail.com>
2024-01-28 16:05:23 +0100

    Add config-server

--- payment-service/src/main/java/pl/majczyna/paymentservice/PaymentApi.java
+++ payment-service/src/main/java/pl/majczyna/paymentservice/PaymentApi.java
@@ -1,13 +1,16 @@
 package pl.majczyna.paymentservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentApi {
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
     @GetMapping()
     public String hello(){
 
-        return "hello world from PaymentApi!";
+        return "hello world from PaymentApi: " + instanceId;
     }
 }
